17906c9b3865649b7be293ddfcfa66f13982b724
Inez <144671961+Pink-Mallow@users.noreply.github.com>
2025-12-17 22:41:13 +0100

    test commit

--- src/lib/components/organisms/Footer.svelte
+++ src/lib/components/organisms/Footer.svelte
@@ -3,17 +3,40 @@
 </script>
 
 <footer>
+  <div class="menu">
+    <label for="">Menu</label>
+    <a href="/">Home</a>
+    <a href="/">Methodes</a>
+    <a href="/">Over AI</a>
+  </div>
+  <div class="socials">
+    <label for="">Socials</label>
+    <a href="/">Instagram</a>
+    <a href="/">LinkedIn</a>
+  </div>
+  <div class="contact">
+    <label for="">Contact</label>
+    <a href="/">janedoe@gmail.com</a>
+  </div>
+  <p>Creating Tomorrow</p>
 
 </footer>
-
+<!-- just a test to commit so i am not scoobydoo -->
 <style>
   footer {
     /* background-color: #f8f9fa; */
-    background-color: red;
+    background-color:darkslategray;
     padding: 1rem;
     text-align: center;
     font-size: 0.875rem;
     color: #6c757d;
   }
 
+  a {
+    cursor: pointer;
+  }
+
+  a:hover {
+    color: violet;
+  }
 </style>
