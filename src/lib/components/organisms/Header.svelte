cf38f7fc2e97a9d16192cacaafd50fd7c84e1a95
Akikosophia <asschermer@hotmail.com>
2025-12-18 00:40:26 +0100

    #23 feat: add basic HTML structure for navigation

---
+++ src/lib/components/organisms/Header.svelte
@@ -0,0 +1,32 @@
+<script>
+  let { data } = $props();
+</script>
+
+<header>
+<button>
+ <svg></svg>
+</button>
+
+<button>
+    <svg></svg>
+   </button>
+   
+
+<div id="menupopover" popover>
+<nav>
+    <ul>
+        <li><a href="/methods"></a></li>
+    </ul>
+</nav>
+</div>
+
+<button>
+    <svg></svg>
+   </button>
+   
+
+</header>
+
+<style>
+
+</style>
