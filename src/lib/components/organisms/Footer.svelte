a1a150280ff22f4ab96c79fa31b65a07a1f8b62e
Inez <144671961+Pink-Mallow@users.noreply.github.com>
2025-12-19 19:24:23 +0100

    #11 Basic layout
    I have created a basic layout for the footer for desktop just to have a clear overview.
    I added:
    - Having added a grid layout.
    - Padding to have some space.

--- src/lib/components/organisms/Footer.svelte
+++ src/lib/components/organisms/Footer.svelte
@@ -7,36 +7,68 @@
     <label for="">Menu</label>
     <a href="/">Home</a>
     <a href="/">Methodes</a>
-    <a href="/">Over AI</a>
+    <a href="/">Over AI</a>  
   </div>
-  <div class="socials">
-    <label for="">Socials</label>
-    <a href="/">Instagram</a>
-    <a href="/">LinkedIn</a>
-  </div>
+  <label for="logo">Creating Tomorrow</label>
   <div class="contact">
     <label for="">Contact</label>
     <a href="/">janedoe@gmail.com</a>
   </div>
-  <p>Creating Tomorrow</p>
+  
 
 </footer>
 
 <style>
   footer {
-    /* background-color: #f8f9fa; */
-    background-color:darkslategray;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 20em;
+
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    
+    background-color: indianred;
     padding: 1rem;
-    text-align: center;
     font-size: 0.875rem;
-    color: #6c757d;
+  }
+
+  label {
+    color: snow;
   }
 
   a {
     cursor: pointer;
   }
 
+  [for="logo"] {
+    align-self: self-end;
+    color: snow;
+    font-style: bold;
+    padding: 1.75em;
+    grid-column: 1 / span 1;
+  }
+
+  .menu {
+    display: grid;
+    padding: 1.75em;
+    height:fit-content;
+  }
+
+  .contact {
+    display: grid;
+    justify-self: end;
+    padding: 1.75em;
+    height:fit-content;
+    grid-column: 3;
+    grid-row: 1;
+  }
+
   a:hover {
     color: violet;
   }
+
+
+
 </style>
