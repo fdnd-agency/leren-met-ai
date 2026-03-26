a0fb3e9bb76d2572c3ccdaa5d65b18e2949445e7
Akikosophia <asschermer@hotmail.com>
2026-03-26 14:59:11 +0100

    #40  feat: add basic HTML structure

---
+++ src/lib/components/organism/Hero.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<header>
+    <nav class="hero-nav">
+        <a href="/"> Home </a>
+    </nav>
+
+    <a href="/methods">Methodes</a>
+
+<h1>
+    <img src="" class="Large-logo" alt="Logo-leren-met-ai">
+</h1>
+
+</header>
+
+<section>
+
+    <h2></h2>
+    <p></p>
+
+</section>
