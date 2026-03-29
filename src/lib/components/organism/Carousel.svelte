9a0d30e376e355d12f3b76d1fe4057e7eea72908
Akikosophia <asschermer@hotmail.com>
2026-03-29 17:22:52 +0200

    #37  feat: implement basic carousel layout

--- src/lib/components/organism/Carousel.svelte
+++ src/lib/components/organism/Carousel.svelte
@@ -1,5 +1,84 @@
-<script>
-    
-</script>
 
-<h1> test</h1>
+
+<section class="carousel-container">
+
+    <button class="prev">
+    </button>
+
+<ul class="carousel">
+    <li class="card">
+        <h3>Beeld Audio</h3>
+        <p>Maakt afbeeldingen en ontwerpen op basis van tekst</p>
+    <div class="labels">
+        <a href="/">label</a>
+        <a href="/">label</a>
+    </div>
+</li>
+
+    <li class="card">
+        <h3>Beeld Audio</h3>
+        <p>Maakt afbeeldingen en ontwerpen op basis van tekst</p>
+    <div class="labels">
+        <a href="/">label</a>
+        <a href="/">label</a>
+    </div>
+</li>
+
+    <li class="card">
+        <h3>Beeld Audio</h3>
+        <p>Maakt afbeeldingen en ontwerpen op basis van tekst</p>
+    <div class="labels">
+        <a href="/">label</a>
+        <a href="/">label</a>
+    </div>
+
+    <li class="card">
+        <h3>Beeld Audio</h3>
+        <p>Maakt afbeeldingen en ontwerpen op basis van tekst</p>
+    <div class="labels">
+        <a href="/">label</a>
+        <a href="/">label</a>
+    </div>
+
+    <li class="card">
+        <h3>Beeld Audio</h3>
+        <p>Maakt afbeeldingen en ontwerpen op basis van tekst</p>
+    <div class="labels">
+        <a href="/">label</a>
+        <a href="/">label</a>
+    </div>
+
+</ul>
+
+
+    <button class="next">
+        
+    </button>
+
+
+</section>
+
+<style>
+
+
+.carousel-container {
+    position: relative;
+    width: 400px;
+}
+
+.carousel {
+    display: flex;
+    scroll-snap-type: x mandatory;
+    overflow-x:auto;
+    width: 300px;
+    gap: 10px;
+}
+
+.card {
+    width: 300px;
+    border: 1px solid black;
+    text-decoration: none;
+}
+ 
+
+</style>
