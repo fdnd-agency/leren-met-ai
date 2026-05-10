4500362c51b35bb22c5ad5e1307e07e195f210fa
Naddybsx <Nadd_bs@icloud.com>
2026-05-11 01:19:26 +0200

    Revert "Feature carousel functionality"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,6 @@
 
   const { methods } = data
 
-  import Carousel from '$lib/components/organism/Carousel.svelte';
   import Hero from '$lib/components/organism/Hero.svelte';
 
 </script>
@@ -13,8 +12,4 @@
 <!-- <h1>Goed onderwijs ontstaat wanneer we kennis delen, ook met AI.</h1>
 <h2>Methode van de week</h2>
 
-<a href="/methods">Bekijk alle methoden</a>
-
-
-<Carousel/>
 <a href="/methods">Bekijk alle methoden</a> -->
