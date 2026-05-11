772e6c1e423267e595a118dfc0576ef9a14d0d0c
Akikosophia <asschermer@hotmail.com>
2026-05-11 02:14:26 +0200

    Revert "Revert "Feature filter component""
    This reverts commit ffa0b61448217faee4a04d2fbe8e986f307ed4f7.

--- src/routes/methods/+page.svelte
+++ src/routes/methods/+page.svelte
@@ -2,10 +2,22 @@
   import MethodsLayout from '$lib/components/organisms/MethodsLayout.svelte';
 
     let { data } = $props()
-  
-    const { methods } = data
+
+    // const { methods } = data
+
+    import Filter from '$lib/components/organism/Filter.svelte';
   
   </script>
+
+  <Filter/>
   
+  <!-- <h2>AI methoden</h2>
+  {#each methods as method}
+    <article>
+      <h3>{method.title}</h3>
+      <p>{method.description}</p>
+      <a href="/methods/{method.slug}">Bekijk {method.title}</a>
+    </article>
+  {/each}   -->
   <h2>AI methoden</h2>
   <MethodsLayout {methods} />
